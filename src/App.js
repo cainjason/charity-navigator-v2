9c42ec414c641a577a27c7a4c84395abaf7c31b7
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-04-15 18:04:30 -0400

    chore: created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 
+
 function App() {
   return (
     <div className="App">
+        <div className="btn-group">
+            <button className="btn btn-active">Button</button>
+            <button className="btn">Button</button>
+            <button className="btn">Button</button>
+        </div>
      <h1> God is Great </h1>
     </div>
   );
